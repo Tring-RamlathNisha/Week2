46aa1f28d678813f97bc75d9da5b0e7d81436eb3
Tring-RamlathNisha <ramlath.n@tringapps.com>
2025-02-19 13:03:05 +0530

    modified

--- Car.java
+++ Car.java
@@ -4,9 +4,9 @@ public class Car extends Vehicle{
  //private boolean isConvertible;
  private String fuelType;
  
- public Car(String type ,String make, String model, int year, double rentalRate, int numDoors, String fuelType) {
+ public Car(String type ,String make, String model, int year, double rentalRate, int numPers, String fuelType) {
   super(type,make, model, year, rentalRate);
-  this.numDoors = numDoors;
+  this.numDoors = numPers;
   //this.isConvertible = isConvertible;
   this.fuelType = fuelType;
  }
@@ -14,7 +14,7 @@ public class Car extends Vehicle{
  @Override
  public void displayInfo() {
   super.displayInfo();
-  System.out.println("Number of Doors: " + numDoors);
+  System.out.println("No. of accomodate persons: " + numDoors);
   //System.out.println("Convertible: " + isConvertible);
   System.out.println("Fuel Type: " + fuelType);
  }
